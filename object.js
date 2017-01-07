6078434cb6162138dc5ea1ef92efb58b301fc931
poonkeegan <poonkeegan@gmail.com>
2017-01-07 14:12:08 -0500

    Added drawing behaviour to classes

--- object.js
+++ object.js
@@ -1,11 +1,12 @@
 
 
-function Vehicle(x, y, w, h, a, accel) {
+function Vehicle(x, y, w, h, a) {
 	this.x = x;
 	this.y = y;
 	this.width = w;
 	this.height = h;
 	this.angle = a;
+	this.accel = 0;
 	this.speed = 0;
 	this.xspeed = function () {
 		return Math.cos(this.angle) * this.speed;
@@ -13,6 +14,16 @@ function Vehicle(x, y, w, h, a, accel) {
 	this.yspeed = function () {
 		return Math.cos(this.angle) * this.speed;
 	};
+	this.update = function() {
+		this.speed += this.accel;
+		this.x += this.xspeed();
+		this.y += this.yspeed();
+	}
+	this.draw = function (){
+		ctx.rotate(angle);
+		ctx.strokeRect(this.x, this.y, this.width, this.height);
+		//Strok Erect
+	}
 
 }
 
@@ -23,10 +34,18 @@ function Road(w, s, p) {
 	this.side = s;
 	// How far from the origin does this road start from
 	this.position = p;
-}
+	this.draw = function (end){
+		if(side == 0){
+			ctx.strokeRect(this.position, 0, this.width, end);
+		}else if (side == 1){
+			ctx.strokeRect(0, this.position, end, this.width);
 
-var vehicle = {
-}
+		}else if (side == 2){
+			ctx.strokeRect(end, this.position, CANVAS_WIDTH - end, this.width);
 
-var road = {
+		}else if (side == 3){
+			ctx.strokeRect(this.position, end, this.width, CANVAS_HEIGHT - end);
+
+		}
+	}
 }
