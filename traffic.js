d63160f174dc426728470772529d0dd1ba476492
SpectrumWings <archmon@hotmail.com>
2017-01-07 13:26:34 -0500

    Delete traffic.js

--- traffic.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.createElement("canvas");
-canvas.width = 640;
-canvas.height = 480;
-var ctx = canvas.getContext("2d");
-ctx.strokeRect(100,100,50,60);
-ctx.strokeRect(0,0,canvas.width,canvas.height);
-document.body.appendChild(canvas);
