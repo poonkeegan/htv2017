dfa62014015d0643a008d686635e35025518404c
poonkeegan <poonkeegan@gmail.com>
2017-01-07 13:32:43 -0500

    Starting vehicle definition

--- object.js
+++ object.js
@@ -1,3 +1,21 @@
+
+
+function Vehicle(x, y, w, h, a, accel) {
+	this.x = x;
+	this.y = y;
+	this.width = w;
+	this.height = h;
+	this.angle = a;
+	this.speed = 0;
+	this.xspeed = function () {
+		return Math.cos(this.angle) * this.speed;
+	};
+	this.yspeed = function () {
+		return Math.cos(this.angle) * this.speed;
+	};
+
+}
+
 var vehicle = {
 }
 
