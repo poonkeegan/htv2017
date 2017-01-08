47057f7a93119dbb3a0060e33ca3ae8ce9f92082
SpectrumWings <archmon@hotmail.com>
2017-01-08 03:28:24 -0500

    asphalt colour

--- main.js
+++ main.js
@@ -154,9 +154,11 @@ function drawCanvas()
 }
 */
 function master(){
-	handleCollision();
+	//handleCollision();
+	ctx.fillStyle = "#00ff3c"
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.strokeRect(0,0,canvas.width,canvas.height);
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+	ctx.fillStyle = "#727272"
 	horizontalRoad.draw(intPoint.x);
 	verticalRoad.draw(intPoint.y);
 	sliderControl();
@@ -170,21 +172,21 @@ function master(){
 	console.log(vehicles);
 	window.requestAnimationFrame(master);
 }
-
+/*
 function handleCollision(){
 	var colliding = collidingCars();
 	// Figure out which vehicles sets aren't colliding anymore
 	var notColliding = collVeh.difference(colliding);
 	for(let i of notColliding){
-		i[1].accel = 1;
+		i[0].accel = 1;
 	}
 	// Figure out which are
 	for(let i of colliding){	
 		//Slow down i[0]
-		i[1].accel = -1;
+		i[0].accel = -1;
 	}
 	collVeh = colliding;
-}
+}*/
 function collidingCars()
 {
 	var colliding = new Set();
@@ -192,7 +194,7 @@ function collidingCars()
 		for (j = i + 1; j < vehicles.length; j++){
 			var xDistSq = (vehicles[i].x - vehicles[j].x) * (vehicles[i].x - vehicles[j].x);
 			var yDistSq = (vehicles[i].y - vehicles[j].y) * (vehicles[i].y - vehicles[j].y);
-			if (xDistSq + yDistSq < 100*100){ // < radius^2 
+			if (xDistSq + yDistSq < 3600){ // < radius^2 (radius = 60)
 				colliding.add([vehicles[i], vehicles[j]]);
 			}
 		}
